90cef64c041658cbbf75761522fa5cd35905422e
Andreas Ink <andreasink17@gmail.com>
2021-02-27 21:09:01 -0500

    Implemented user reports

--- MapApp/Views/Home/HomeView.swift
+++ MapApp/Views/Home/HomeView.swift
@@ -7,7 +7,8 @@
 
 import SwiftUI
 import MapKit
-
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 struct HomeView: View {
     
     @State var region = MKCoordinateRegion(
@@ -30,6 +31,17 @@ struct HomeView: View {
                         region = locationManager.currentRegion ?? MKCoordinateRegion(
                             center: CLLocationCoordinate2D(latitude: 25.7617, longitude: 80.1918),
                             span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10))
+                            
+                            self.loadNearby() { userData in
+                                //Get completion handler data results from loadData function and set it as the recentPeople local variable
+                                for data in userData {
+                                    self.reports.append(data)
+                                }
+                                reports = reports.removeDuplicates()
+                            }
+                        }
+                      
+                        
                         }
                     }
                 MapView(region: $region, route: $mkRoute, reports: $reports)
@@ -48,6 +60,47 @@ struct HomeView: View {
                     BottomView(route: $locationManager.route, mkRoute: $mkRoute, locationManager: locationManager, landmark: $landmark, reports: $reports)
                 }.edgesIgnoringSafeArea(.all)
             }
+        }
+    
+    func loadNearby(performAction: @escaping ([Report]) -> Void) {
+        let db = Firestore.firestore()
+     let docRef = db.collection("reports")
+        var userList:[Report] = []
+        //Get every single document under collection users
+        let lat = 0.0144927536231884
+            let lon = 0.0181818181818182
+
+        let lowerLat = locationManager.currentLocation.coordinate.latitude - (lat * 10)
+            let lowerLon = locationManager.currentLocation.coordinate.longitude - (lon * 10)
+
+            let greaterLat = locationManager.currentLocation.coordinate.latitude + (lat * 10)
+            let greaterLon = locationManager.currentLocation.coordinate.longitude + (lon * 10)
+
+            let lesserGeopoint = GeoPoint(latitude: lowerLat, longitude: lowerLon)
+            let greaterGeopoint = GeoPoint(latitude: greaterLat, longitude: greaterLon)
+        let query = docRef.whereField("location", isLessThan: greaterGeopoint)
+        query.getDocuments { (documents, error) in
+           
+        for document in documents!.documents {
+                let result = Result {
+                    try document.data(as: Report.self)
+                }
+                switch result {
+                    case .success(let user):
+                        if let user = user {
+                            userList.append(user)
+                 
+                        } else {
+                            
+                            print("Document does not exist")
+                        }
+                    case .failure(let error):
+                        print("Error decoding user: \(error)")
+                    }
+     
+        
+            }
+              performAction(userList)
         }
     }
 }
